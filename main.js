8279b25cded41d7520472a180083fd8968dc7bb8
Cristian Corrales <cristiandavid.corrales@gmail.com>
2022-12-26 21:59:11 -0500

    Se finalizaron los menu mobile y desktop y se agregó la funcionalidad abrir y cerrar el menu mobile

---
+++ main.js
@@ -0,0 +1,14 @@
+const menuMobile = document.querySelector(".mobile-menu");
+const iconMobileMenu = document.querySelector(".mobile-icon-nav-container")
+const iconCloseMobileMenu = document.querySelector(".close-icon-container");
+
+iconMobileMenu.addEventListener("click", openMobileMenu);
+iconCloseMobileMenu.addEventListener("click", closeMobileMenu)
+
+function openMobileMenu() {
+    menuMobile.classList.remove("inactive");
+}
+
+function closeMobileMenu() {
+    menuMobile.classList.add("inactive");
+}
