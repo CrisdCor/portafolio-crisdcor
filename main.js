58292afc96ee56aa87557b42921087b212ac16b3
Cristian Corrales <cristiandavid.corrales@gmail.com>
2023-01-08 00:46:42 -0500

    Se agrega secciones CONTACTO y HOME

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const menuMobile = document.querySelector(".mobile-menu");
-const iconMobileMenu = document.querySelector(".mobile-icon-nav-container")
+const iconMobileMenu = document.querySelector(".mobile-icon-nav-container");
 const iconCloseMobileMenu = document.querySelector(".close-icon-container");
 
+
+
 iconMobileMenu.addEventListener("click", openMobileMenu);
-iconCloseMobileMenu.addEventListener("click", closeMobileMenu)
+iconCloseMobileMenu.addEventListener("click", closeMobileMenu);
+
+
 
 function openMobileMenu() {
     menuMobile.classList.remove("inactive");
 }
+
 
 function closeMobileMenu() {
     menuMobile.classList.add("inactive");
